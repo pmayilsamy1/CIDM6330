e61d5dd202c3981b8f9360135ce8da5cd72ce574
Priya Mayilsamy <priyamayilsamy@Priyas-MBP.attlocal.net>
2023-05-10 21:15:29 -0500

    Added tests

--- projects/Barky/tests/unit/test_handlers.py
+++ projects/Barky/tests/unit/test_handlers.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from datetime import date
+from datetime import date, datetime, timedelta
 from typing import Dict, List
 
 import pytest
@@ -27,3 +27,49 @@ class FakeRepository(repository.AbstractRepository):
             (p for p in self._products for b in p.batches if b.reference == batchref),
             None,
         )
+
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self):
+        self.services = FakeRepository([])
+        self.committed = False
+
+    def _commit(self):
+        self.committed = True
+
+    def rollback(self):
+        pass
+
+class FakeNotifications(notifications.AbstractNotifications):
+    def __init__(self):
+        self.sent = defaultdict(list)  # type: Dict[str, List[str]]
+
+    def send(self, destination, message):
+        self.sent[destination].append(message)
+
+def bootstrap_test_app():
+    return bootstrap.bootstrap(
+        start_orm=False,
+        uow=FakeUnitOfWork(),
+        notifications=FakeNotifications(),
+        publish=lambda *args: None,
+    )
+
+class TestAddBookmark:
+    def test_add_bookmark(self):
+        bus = bootstrap_test_app()
+        created = datetime.now().isoformat()
+        edited = created
+        bus.handle(commands.AddBookmarkCommand(0, "test", "http://www.example/com", None, created, edited))
+        assert bus.uow.services.get("HairCut") is not None
+        assert bus.uow.committed
+
+def test_for_existing_bookmark(self):
+        bus = bootstrap_test_app()
+        created = datetime.now().isoformat()
+        edited = created
+        bus.handle(commands.AddBookmarkCommand(0, "test", "http://www.example/com", None, created, edited)))
+        bus.handle(commands.AddBookmarkCommand(0, "test", "http://www.example/com", None, created, edited)))
+        assert "test" in [
+            b.service_name for b in bus.uow.services.get("test").bookmarks
+        ]
+
